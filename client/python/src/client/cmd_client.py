34e0edf5e833d918242b82561c14a547a4600e0b
zero88 <sontt246@gmail.com>
2021-04-17 02:22:53 +0700

    - Add hash version - GHA: add inject hash version - GHA: add distribute step

--- client/python/src/client/cmd_client.py
+++ client/python/src/client/cmd_client.py
@@ -12,7 +12,7 @@ import click
 
 import src.utils.logger as logger
 from src.client.device_resolver import DeviceResolver, DHCPReason
-from src.client.version import CLI_VERSION
+from src.client.version import CLI_VERSION, HASH_VERSION
 from src.executor.shell_executor import SystemHelper
 from src.executor.vpn_cmd_executor import VpnCmdExecutor
 from src.utils.constants import ErrorCode, Versions, AppEnv
@@ -567,8 +567,9 @@ def __log(vpn_opts: ClientOpts, date, lines, follow, another):
 @vpn_client_opts
 @dev_mode_opts(opt_name=ClientOpts.OPT_NAME)
 def __version(vpn_opts: ClientOpts):
-    logger.info('VPN version: %s', vpn_opts.get_vpn_version(Versions.VPN_VERSION))
-    logger.info('CLI version: %s', CLI_VERSION)
+    logger.info('VPN version : %s', vpn_opts.get_vpn_version(Versions.VPN_VERSION))
+    logger.info('CLI version : %s', CLI_VERSION)
+    logger.info('Hash version: %s', HASH_VERSION)
 
 
 @cli.command(name='command', help='Execute Ad-hoc VPN command', hidden=True)
