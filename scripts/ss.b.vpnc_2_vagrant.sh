5e7253e6444e1da86f9ce4ec4de296f84dead189
zero88 <sontt246@gmail.com>
2021-04-23 19:42:40 +0700

    - Add download option in build script

--- scripts/ss.b.vpnc_2_vagrant.sh
+++ scripts/ss.b.vpnc_2_vagrant.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+DOWNLOAD=${1:-false}
 cur="$PWD"
+
 pipenv install
 cd client/python
+if [[ "$DOWNLOAD" == "true" ]]; then
+    V=$(python -c "from src.utils.constants import Versions; print (Versions.VPN_VERSION)") \
+        && pipenv run python -m src.client.cmd_client download -cv "$V"
+fi
 pipenv run pyinstaller src/client/cmd_client.py -n qweio-vpnc --clean --onefile --add-data src/client/resources/*:resources/
 cp -rf dist/qweio-vpnc "$cur/vagrant/shared"
 cd -
