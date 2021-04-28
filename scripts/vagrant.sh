87d39250eb22360dbafed6136bafd4d42fd8a68c
zero88 <sontt246@gmail.com>
2021-04-28 12:12:38 +0700

    - Update vagrant.sh to allow up/destroy/halt/status/port command

---
+++ scripts/vagrant.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+cur="$PWD"
+
+COMMAND=(up halt destroy status port)
+
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+cmd=$1
+
+if [[ ! " ${COMMAND[*]} " =~ " $cmd " ]]; then
+    c=$(
+        IFS=/
+        echo "${COMMAND[*]}"
+    )
+    echo -e "${RED}Unsupported [$cmd]. One of command [${c}] ${NC}"
+    exit 10
+fi
+
+shift
+
+vagrant_opt=("$cmd")
+if [[ $cmd == "destroy" && $FORCE == "1" ]]; then
+    vagrant_opt+=("-f")
+fi
+
+for machine in "$@"; do
+    if [[ -f "$cur/vagrant/$machine/Vagrantfile" ]]; then
+        cd "$cur/vagrant/$machine"
+        echo -e "${BLUE}Vagrant $cmd $machine....${NC}"
+        vagrant "${vagrant_opt[@]}"
+        cd - >/dev/null
+        echo "=================================================================================="
+    else
+        echo -e "${YELLOW}Not found machine [$machine] ${NC}"
+        echo "=================================================================================="
+    fi
+done
